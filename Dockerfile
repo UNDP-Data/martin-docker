39fe550ea1c1c7475b25d64ebea30b0ae71a9bd8
janf <ioan.ferencik@solargis.com>
2021-12-23 18:20:47 +0100

    replace sed usage with envsubst inside etrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM urbica/martin
+
 RUN apk add curl
+RUN apk add gettext
+
 RUN mkdir -p /etc/martin
 COPY entrypoint.sh /etc/martin/entrypoint.sh
 WORKDIR /etc/martin
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-# inject the nev variables into the config file using envsubst
-config='/etc/martin/config.yaml'
-tmp_config='/etc/martin/tmp_config.yaml'
-envsubst < $config > $tmp_config
-mv $tmp_config $config
-martin --config $config
+set -e
+# inject the env variables into the config file using envsubst
+CONFIG='/etc/martin/config.yaml'
+
+if grep -Fq "\$DATABASE_URL" $CONFIG
+then
+    TMP_CONFIG='/etc/martin/tmp_config.yaml'
+    envsubst < $CONFIG > $TMP_CONFIG
+    cat $TMP_CONFIG > $CONFIG
+    rm -rf $TMP_CONFIG
+fi
+
+martin --config $CONFIG

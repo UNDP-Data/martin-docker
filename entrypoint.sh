21a92084f25aa58031f7b8fe58f33a787133cf6b
janf <ioan.ferencik@solargis.com>
2021-12-24 00:40:23 +0100

    updated docker-compose to use latest tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 # inject the env variables into the config file using envsubst
-CONFIG='/etc/martin/config.yaml'
+CONFIG="/etc/martin/config.yaml"
 
 if grep -Fq "\$DATABASE_URL" $CONFIG
 then
-    TMP_CONFIG='/etc/martin/tmp_config.yaml'
+    TMP_CONFIG="/etc/martin/tmp_config.yaml"
     envsubst < $CONFIG > $TMP_CONFIG
     cat $TMP_CONFIG > $CONFIG
     rm -rf $TMP_CONFIG

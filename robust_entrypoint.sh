8b94425bbc91fe8ccdf3d4f0924e1171ae752452
janf <ioan.ferencik@solargis.com>
2022-01-05 15:19:06 +0100

    add robust_entrypoint.sh

---
+++ robust_entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+
+
+if [ -z "$DATABASE_URL" ] && [ -z ${DATABASE_URL} ]
+then
+  echo "DATABASE_URL is not set. The vector tile server can not connect to PostGIS server."
+  exit 1
+fi
+
+
+
+# inject the env variables into the config file using envsubst
+CONFIG="/etc/martin/config.yaml"
+
+#extract the value from config.yaml
+DBURL=$(grep -R "\$DATABASE_URL" $CONFIG |  awk '/:/ {print $2}')
+
+#if it is not set
+if [ "$DBURL" = "'\$DATABASE_URL'" ]
+then
+    echo "Interpolating env vars into $CONFIG ..."
+    TMP_CONFIG="./tmp_config.yaml"
+    envsubst < $CONFIG > $TMP_CONFIG
+    cat $TMP_CONFIG > $CONFIG
+    rm -rf $TMP_CONFIG
+else
+    echo "$CONFIG is already interpolated and will be reused ..."
+fi
+
+#run the server using the config
+martin --config $CONFIG
